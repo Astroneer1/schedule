2b3ad26ebf30cf23e0259efe73bc24043fad37bf
Astroneer1 <15jy0124@gmail.com>
2019-05-19 15:12:57 +0900

    first commit

--- src/controllers/EventController.go
+++ src/controllers/EventController.go
@@ -18,22 +18,16 @@ func (ec EventController) EventIndex(c *gin.Context) {
 
 
 func (ec EventController) EventCreate (c *gin.Context) {
-	var eventNameErr error
-
 	db,_ := gorm.Open("mysql", "root:mysql@/schedule?charset=utf8&parseTime=True&loc=Local")
 
 	eventName := c.PostForm("eventName")
 	memo := c.PostForm("memo")
 	days := c.PostForm("days")
 
-	if eventName != "" {
-		event := models.Event{}
-		event.EventName = eventName
-		event.Memo = memo
-		db.Create(&event)
-	} else {
-		eventNameErr := errors.New("イベントを入力してください！")
-	}
+	event := models.Event{}
+	event.EventName = eventName
+	event.Memo = memo
+	db.Create(&event)
 
 	fmt.Println(event.EventID)
 	fmt.Println(event.Memo)
@@ -42,9 +36,5 @@ func (ec EventController) EventCreate (c *gin.Context) {
 
 	defer db.Close()
 
-	if eventNameErr = nil {
-		c.HTML(http.StatusOK, "Schedule.tmpl", gin.H{"eventNameErr": eventNameErr, "memo": memo, "day": days})
-	} else {
-		c.HTML(http.StatusOK, "Schedule.tmpl", gin.H{"eventName": eventName, "memo": memo, "day": days})
-	}
+	c.HTML(http.StatusOK, "Schedule.tmpl", gin.H{"eventName": eventName, "memo": memo, "day": days})
 }
